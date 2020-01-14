c4f664a4716185f42258e59c7902374bbd45463b
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2020-01-14 22:59:22 +0100

    Committed: Tue, Jan 14, 2020 10:59:22 PM

--- fanfiction/fanfiction/spiders/books.py
+++ fanfiction/fanfiction/spiders/books.py
@@ -2,13 +2,18 @@ import scrapy
 
 class Books(scrapy.Spider):
     name='books'
-
     start_urls = ['https://www.fanfiction.net/book/']
     
     def parse(self,response):
         book_list = dict()
+        i=0
         for book in response.xpath("//div[@id='list_output']//a"):
-            book_list.update(book.xpath('@title').extract(),book.xpath('@href').extract()[0])
+            i=i+1
+            if(i>10):
+                break
+            title = str(book.xpath('@title').extract())
+            link = str(book.xpath('@href').extract()[0])
+            book_list[title] = link
 
-        print(book_list)    
+           
         yield book_list
