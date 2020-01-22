8f0872099abbf0578340c4229c2b7bb90a9600a9
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2020-01-22 23:00:25 +0100

    Committed: Wed, Jan 22, 2020 11:00:24 PM

--- fanfiction/fanfiction/items.py
+++ fanfiction/fanfiction/items.py
@@ -8,7 +8,8 @@
 import scrapy
 
 
-class FanfictionItem(scrapy.Item):
+class BookItems(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    link = scrapy.Field()
+    
--- fanfiction/fanfiction/spiders/books.py
+++ fanfiction/fanfiction/spiders/books.py
@@ -1,20 +1,30 @@
 import scrapy
+from ..items import BookItems
+import json
+
 
 class Books(scrapy.Spider):
-    name='books'
+    name = 'books'
     start_urls = ['https://www.fanfiction.net/book/']
-    
-    def parse(self,response):
+
+    def parse(self, response):
+        i = 0
         book_list = dict()
-        i=0
+        # Get 10 most popular books
         for book in response.xpath("//div[@id='list_output']//a"):
-            i=i+1
-            if(i>10):
+            i = i+1
+            if(i > 10):
                 break
-            title = str(book.xpath('@title').extract())
+            title = str(book.xpath('@title').extract()[0])
             link = str(book.xpath('@href').extract()[0])
             book_list[title] = link
 
-           
+        f = open("books.json", "w")
+        f.write(json.dumps(book_list))
+        f.close()
+
+        for title in book_list:
+            print("Title:",title)
+            print("URL:",book_list[title]) 
+
         yield book_list
-        
