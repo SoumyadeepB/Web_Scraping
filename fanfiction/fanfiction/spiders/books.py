8003e77f443688765e308d434be9c0e59f310b15
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2020-01-23 03:18:39 +0100

    Committed: Thu, Jan 23, 2020  3:18:39 AM

--- fanfiction/fanfiction/spiders/books.py
+++ fanfiction/fanfiction/spiders/books.py
@@ -1,5 +1,5 @@
 import scrapy
-from ..items import BookItems
+from scrapy import Request
 import json
 
 
@@ -24,7 +24,37 @@ class Books(scrapy.Spider):
         f.close()
 
         for title in book_list:
-            print("Title:",title)
-            print("URL:",book_list[title]) 
+            url = book_list[title]
+            #print("URL:", response.urljoin(url))
+            yield Request(url=response.urljoin(url), callback=self.reviews, meta={'title': title})
 
-        yield book_list
+    def reviews(self, response):
+        review_list = {}  # Review Dictionary
+        book_title = response.meta['title']
+        # List of tags for each element (To allow a JSON dump)
+        review_list[book_title] = []
+        titles = []
+        authors = []
+        texts = []
+        print("* Review for :: ", book_title)
+
+        for title in response.xpath('//*[@id="content_wrapper_inner"]/div[position()>3]/a[1]/text()').extract():
+            titles.append(title)
+
+        for author in response.xpath('//*[@id="content_wrapper_inner"]/div[position()>3]/a[3]/text()').extract():
+            authors.append(author)
+
+        for text in response.xpath('//*[@id="content_wrapper_inner"]/div[position()>3]/div/text()').extract():
+            texts.append(text)
+
+        limit = 15
+        for i in range(limit):
+            review_list[book_title].append({
+                'title': titles[i],
+                'author': authors[i],
+                'text': texts[i]
+            })
+        f = open(book_title+".json", "w")
+        f.write(json.dumps(review_list[book_title]))
+        f.close()
+        yield review_list
